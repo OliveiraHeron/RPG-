5e93bfd197d445d14cae1e43700f954d143aa212
Heron Oliveira <heron.fiho@cs.up.edu.br>
2025-08-19 19:25:46 -0300

    Adicionei a classe Personagem e corrigi bug de mana

---
+++ Pensonagem.py
@@ -0,0 +1,20 @@
+
+
+from abc import ABC, abstractmethod
+
+class Personagem:
+    def __init__(self, nome, classe, vida, forca):
+        self.nome=nome
+        self.classe=classe
+        self.vida=vida
+        self.forca=forca            
+
+heroi= Personagem("Heron","mago",100,50)        
+
+
+print(heroi.nome)
+print(heroi.classe)
+print(heroi.vida)
+print(heroi.forca)
+
+
